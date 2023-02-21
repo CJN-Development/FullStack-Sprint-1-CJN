827f3a7199ec00e5c02569fd6d714395edf418c3
CamDamico <camerondamico2001@gmail.com>
2023-02-21 13:14:26 -0330

    finished css in server

--- server.js
+++ server.js
@@ -29,16 +29,60 @@ const server = http.createServer(async (req, res) => {
                     <!doctype html>
                     <html>
                     <head>
-                    <style>
-                    a{
-                      font-size: 32px;
-                    }
-                    </style>
-                   
+                      <style>
+                        *{
+                          padding: 0;
+                          margin: 0;
+                          box-sizing: border-box;
+                        }
+
+                        body {
+                          background: rgb(2, 0, 36);
+                          background: linear-gradient(
+                            90deg,
+                            rgba(2, 0, 36, 1) 0%,
+                            rgba(9, 9, 121, 1) 35%,
+                            rgba(0, 212, 255, 1) 100%
+                          );
+                          padding: 0;
+                          margin: 0;
+                        }
+
+                        header {
+                          size: 30px;
+                          font-size: 25px;
+                          background-color: black;
+                          color: white;
+                          font-family: 'Poppins', sans-serif;
+                          padding: 10px;
+                        }
+                  
+                        #headerLink {
+                          text-decoration: none;
+                          color: white;
+                          padding: 5;
+                        }
+
+                        a:hover {
+                          font-size: 40px;
+                        }
+
+                        div {
+                          color: white;
+                          display: flex;
+                          justify-content: center;
+                          padding-top: 10vh;
+                          font-size: 40px;
+                        }
+                      </style>
                     </head>
                     <body>
-                        ${result.username} token is ${theToken} <br />
-                        <a href="http://localhost:3000">[home]</a>
+                    <header>
+                    <a id="headerLink" class="home" href="http://localhost:3000">Home</a>
+                    </header>
+                    <div>
+                        ${result.username} token is ${theToken} 
+                    </div>
                     </body>
                     </html>
                 `);
@@ -55,9 +99,61 @@ const server = http.createServer(async (req, res) => {
       res.end(`
                 <!doctype html>
                 <html>
+                <head>
+                  <style>
+                *{
+                  padding: 0;
+                  margin: 0;
+                  box-sizing: border-box;
+                }
+
+                body {
+                  background: rgb(2, 0, 36);
+                  background: linear-gradient(
+                    90deg,
+                    rgba(2, 0, 36, 1) 0%,
+                    rgba(9, 9, 121, 1) 35%,
+                    rgba(0, 212, 255, 1) 100%
+                  );
+                  padding: 0;
+                  margin: 0;
+                }
+
+                header {
+                  size: 30px;
+                  font-size: 25px;
+                  background-color: black;
+                  color: white;
+                  font-family: 'Poppins', sans-serif;
+                  padding: 10px;
+                }
+          
+                #headerLink {
+                  text-decoration: none;
+                  color: white;
+                  padding: 5;
+                }
+
+                a:hover {
+                  font-size: 40px;
+                }
+
+                div {
+                  color: white;
+                  display: flex;
+                  justify-content: center;
+                  padding-top: 10vh;
+                  font-size: 40px;
+                }
+                </style>
+              </head>
                 <body>
+                <header>
+                    <a id="headerLink" class="home" href="http://localhost:3000">Home</a>
+                </header>
+                <div>
                     Token count is ${theCount} <br />
-                    <a href="http://localhost:3000">[home]</a>
+                    <div>
                 </body>
                 </html>
             `);
@@ -80,7 +176,7 @@ const server = http.createServer(async (req, res) => {
   }
 });
 server.listen(3000);
-console.log("Running on port 3000")
+console.log("Running on port 3000");
 
 function collectRequestData(request, callback) {
   const FORM_URLENCODED = "application/x-www-form-urlencoded";
